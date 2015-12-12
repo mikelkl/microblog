f823f3d6f7532f9d2bebfc0d879f99b7fee9cedc
Mike Liu <mikelkl@foxmail.com>
2015-12-12 16:01:52 +0800

    version 1

---
+++ README.md
@@ -0,0 +1,1 @@
+# 学习Python微型框架Flask的第一个大型项目----microblog
---
+++ config.py
@@ -0,0 +1,36 @@
+import os
+from my_email_config import MAIL_USERNAME, MAIL_PASSWORD, ADMINS as my_admins
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-nerver-guess'
+
+# mail server settings
+MAIL_SERVER = 'smtp.sina.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = MAIL_USERNAME
+MAIL_PASSWORD = MAIL_PASSWORD
+
+# administrator list
+ADMINS = my_admins
+
+# pagination
+POSTS_PER_PAGE = 3
+
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+MAX_SEARCH_RESULTS = 50
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
