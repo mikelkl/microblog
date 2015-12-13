1d0c836fcc968300f2be1d70afb2c75611431c37
Mike Liu <mikelkl@foxmail.com>
2015-12-13 12:17:16 +0800

    add I18n and L10n

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from my_email_config import MAIL_USERNAME, MAIL_PASSWORD, ADMINS as my_admins
 
@@ -26,6 +27,13 @@ POSTS_PER_PAGE = 3
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
 MAX_SEARCH_RESULTS = 50
+
+# avaliable languages
+LANGUAGES = {
+    'en': 'English',
+    'es': 'Espaè´–ol',
+    'zh': '√Ê ∏',
+}
 
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
